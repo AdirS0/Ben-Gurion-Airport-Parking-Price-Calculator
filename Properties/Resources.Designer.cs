97f0c74fd489c92f3466451481e305abcbd16619
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-07-08 19:32:22 +0300

    updated app

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BenGurionAirportParkingPriceCalculator.Properties {
+namespace Project1.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BenGurionAirportParkingPriceCalculator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BenGurionAirportParkingPriceCalculator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project1.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
